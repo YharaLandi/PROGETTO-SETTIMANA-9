19f5712beaeafb297f9d0725f6d74d58298d7528
YharaLandi <landiyhara@gmail.com>
2026-07-03 11:04:23 +0200

    COmponents vuote + prove render footer e nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css'
+import MyNav from './components/MyNav'
+import MyFooter from './components/MyFooter'
 
 function App() {
+  return (
+    <>
+      <MyNav />
+      <MyFooter />
+    </>
+  )
 }
 
 export default App
