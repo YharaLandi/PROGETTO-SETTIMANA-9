95d22d0fdd0fb37824880fe3708fa98c47e338ea
YharaLandi <landiyhara@gmail.com>
2026-07-03 11:20:39 +0200

    Messaggio di errore e componente ricerca vuoto

---
+++ src/assets/components/Error.jsx
@@ -0,0 +1,7 @@
+import { Alert } from 'react-bootstrap'
+//Gestiamo graficamente 
+const Error = () => (
+  <Alert variant="danger">May the fetch be with you... it wasn't</Alert>
+)
+
+export default Error
