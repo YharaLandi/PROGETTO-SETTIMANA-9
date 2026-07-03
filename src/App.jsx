526716e5ba420a2da5c45a60bc27bc97d0a1de4a
YharaLandi <landiyhara@gmail.com>
2026-07-03 11:04:47 +0200

    prove render in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import MyNav from './components/MyNav'
-import MyFooter from './components/MyFooter'
+import MyNav from './assets/components/MyNav'
+import MyFooter from './assets/components/MyFooter'
 
 function App() {
   return (
     <>
       <MyNav />
+      <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cum, rerum, eveniet quas optio iusto quibusdam quis, aspernatur vero magnam sit pariatur quae numquam excepturi facilis maxime dolorem. Ipsa, tenetur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cum, rerum, eveniet quas optio iusto quibusdam quis, aspernatur vero magnam sit pariatur quae numquam excepturi facilis maxime dolorem. Ipsa, tenetur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cum, rerum, eveniet quas optio iusto quibusdam quis, aspernatur vero magnam sit pariatur quae numquam excepturi facilis maxime dolorem. Ipsa, tenetur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cum, rerum, eveniet quas optio iusto quibusdam quis, aspernatur vero magnam sit pariatur quae numquam excepturi facilis maxime dolorem. Ipsa, tenetur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cum, rerum, eveniet quas optio iusto quibusdam quis, aspernatur vero magnam sit pariatur quae numquam excepturi facilis maxime dolorem. Ipsa, tenetur?</div>
       <MyFooter />
     </>
   )
